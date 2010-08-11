c46a6801df8841e25d8ff5228f0a977f1bb8ad27
xenon.tm <xenon.tm@e9d20148-6181-67a3-abff-1cde03f9fdaf>
2010-08-11 19:16:57 +0000

    чо-то попрвили

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include "piece.h"
 #include "side.h"
 #include "camera.h"
 #include "helloworld.h"
@@ -12,8 +13,7 @@
 t_camera camera;
 int v = 0;
 
-int Times;
-/*
+
 t_piece p[] = {
 	{-1, -1, 1,   0.0,   0.0,180.0, {1.0, 1.0, 1.0}, {1.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, 3},
 	{ 0, -1, 1,   0.0,   0.0,180.0, {1.0, 1.0, 1.0}, {1.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, 2},
@@ -55,7 +55,8 @@ t_piece p[] = {
 	{ 1,  1,-1,   0.0, 180.0, 90.0, {1.0, 1.0, 0.0}, {1.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, 3}
 
 };
-*/
+
+/*
 t_side p[] = {
 {0, {{0,1,0}, {0,1,0}, {0,1,0}, {1,1,1}, {1,1,1}, {1,1,1}, {1,1,1}, {1,1,1}, {1,1,1}}},
 {1, {{1,1,1}, {1,1,1}, {1,1,1}, {0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}}},
@@ -102,8 +103,8 @@ void renderScene(void) {
 		renderBitmapCharacher(0,1.8,0,GLUT_BITMAP_8_BY_13,(const unsigned char *)s);
 	glPopMatrix();
 	
-//	render_pieces(p, 0.5, 0.45);
-	render_sides(p, 0.5, 0.45);
+	render_pieces(p, 0.5, 0.45);
+//	render_sides(p, 0.5, 0.45);
 
 	glutSwapBuffers();
 	
@@ -142,17 +143,17 @@ void keyPressed(unsigned char key, int x, int y) {
 	if (key == '1') camera_move(&camera,  0.0, 0.0,-1.0);
 	if (key == '3') camera_move(&camera,  0.0, 0.0, 1.0);
 
-	if (key == 'a') camera_rotate(&camera,  0.0,-1.0);
-	if (key == 'd') camera_rotate(&camera,  0.0, 1.0);
-	if (key == 'q') camera_rotate(&camera, -1.0, 0.0);
-	if (key == 'z') camera_rotate(&camera,  1.0, 0.0);
+	if (key == 'a') camera_rotate(&camera,  0.0,-4.0);
+	if (key == 'd') camera_rotate(&camera,  0.0, 4.0);
+	if (key == 'q') camera_rotate(&camera, -4.0, 0.0);
+	if (key == 'z') camera_rotate(&camera,  4.0, 0.0);
 
-	if (key == 'u') rotate_sides(0, (t_side*)&p);
-	if (key == 'i') rotate_sides(2, (t_side*)&p);
-	if (key == 'o') rotate_sides(4, (t_side*)&p);
-	if (key == 'j') rotate_sides(1, (t_side*)&p);
-	if (key == 'k') rotate_sides(3, (t_side*)&p);
-	if (key == 'l') rotate_sides(5, (t_side*)&p);
+	if (key == 'u') rotate_pieces(0, (t_piece*)&p);
+	if (key == 'i') rotate_pieces(2, (t_piece*)&p);
+	if (key == 'o') rotate_pieces(4, (t_piece*)&p);
+	if (key == 'j') rotate_pieces(1, (t_piece*)&p);
+	if (key == 'k') rotate_pieces(3, (t_piece*)&p);
+	if (key == 'l') rotate_pieces(5, (t_piece*)&p);
 /*
 	if (key == 'v') {
 		p[PDEB].rx += 90.0;
